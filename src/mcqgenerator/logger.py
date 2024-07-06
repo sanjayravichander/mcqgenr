3596012170d5789c16a1b082b64d858c9224fe75
sanjayravichander <sanjay.1991999@gmail.com>
2024-07-07 00:45:58 +0530

    Updated

--- src/mcqgenerator/logger.py
+++ src/mcqgenerator/logger.py
@@ -18,5 +18,5 @@ LOG_FILEPATH=os.path.join(log_path,LOG_FILE)
 logging.basicConfig(
     level=logging.INFO,
     filename=LOG_FILEPATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(messages)s"
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
 )
